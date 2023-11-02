84d16cc2e0062335a09e2f2d9e527fde3684cfc8
Bekzat <esimhanuli13@gmail.com>
2023-11-02 17:21:20 +0600

    collection logic with api req

--- client/src/pages/Collection.jsx
+++ client/src/pages/Collection.jsx
@@ -1,11 +1,20 @@
 import axios from 'axios';
 import React, { useContext, useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { Error } from '../App';
 import Navbar from '../components/Navbar';
+import Button from '../utils/Button';
+import Input from '../utils/Input';
 
 function Collection() {
     const [collection, setCollection] = useState({});
+    const [isOwner, setIsOwner] = useState(false);
+    const [itemdata, setItemData] = useState({
+        topic:"",
+        desc:""
+    });
+    const [tags, setTags] = useState([]);
+
     const collectionId = useParams();
     
     const {setError, url} = useContext(Error)
@@ -21,9 +30,39 @@ function Collection() {
             })
     }
 
+    const getLoggedUser = () =>{
+        const user = JSON.parse(localStorage.getItem('currentUser'));
+        if(user && user._id === collection.userId){
+            setIsOwner(true);
+        }else{
+            setIsOwner(false);
+        }
+    }
+
+    const addNewItem = () =>{
+        setItemData({...itemdata, userId: collection.userId, tags});
+        axios.put(`${url}/additem/${collection.id}`, {itemdata})
+            .then((res)=>{
+                console.log(res.data);
+                setError(res.data.message);
+            })
+            .catch((e)=>{
+                console.log(e);
+                setError(e.response.data.message);
+            })
+    }
+
+    const handleData = (e) =>{
+        const {name, value} = e.target;
+
+        setItemData({...itemdata, [name]: value});
+    }
+
     useEffect(()=>{
         getCollectionById();
+        getLoggedUser();
     })
+
   return (
     <div>
         <Navbar/>
@@ -32,9 +71,20 @@ function Collection() {
         <p>{collection.theme}</p>
         {collection.items && collection.items.map((item)=>{
             return (
-                <p>{item.topic}</p>
+                <div>
+                    <p>{item.topic}</p>
+                    <Link to={`/item/${item._id}`}></Link>
+                </div>
             )
         })}
+        { isOwner && (
+            <div>
+                <Input onChange={handleData} name="topic" />
+                <Input onChange={handleData} name="desc" />
+                <Input onChange={handleData} name="tags" />
+                <Button name="Add new Item" style="bg-lime-600" onClick={addNewItem}/>
+            </div>        
+        )}
     </div>
   )
 }
