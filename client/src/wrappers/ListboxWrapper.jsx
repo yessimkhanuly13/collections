ae21ab12210426bc648d1cbb4ad856debc517deb
Bekzat Yessimkhanuly <esimhanuli13@gmail.com>
2023-11-18 11:56:05 +0600

    wrapper change

--- client/src/wrappers/ListboxWrapper.jsx
+++ client/src/wrappers/ListboxWrapper.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 export const ListboxWrapper = ({children}) => (
-  <div className="w-full px-1 py-2 dark:">
+  <div className="w-full px-1 py-2">
     {children}
   </div>
 );
